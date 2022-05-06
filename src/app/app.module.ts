ae7c03a74f07784edb8283549c624b74aba28efb
Alejandro Cruz <AlejandroCruz@users.noreply.github.com>
2022-05-06 19:14:30 -0400

    Component app-assignments.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
-import { MatButtonModule } from '@angular/material'
+import { MatButtonModule } from '@angular/material';
+import { AssignmentsComponent } from './assignments/assignments.component'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AssignmentsComponent
   ],
   imports: [
     BrowserModule,
