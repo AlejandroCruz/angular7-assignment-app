f2734f33ba284b293fb73e7ebae527304ba94409
Alejandro Cruz <AlejandroCruz@users.noreply.github.com>
2022-05-07 15:12:45 -0400

    Attribute Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { MatButtonModule } from '@angular/material';
-import { AssignmentsComponent } from './assignments/assignments.component'
+import { AssignmentsComponent } from './assignments/assignments.component';
+import { SubmittedDirective } from './shared/submitted.directive';
+import { UnsubmittedDirective } from './shared/unsubmitted.directive'
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AssignmentsComponent
+    AssignmentsComponent,
+    SubmittedDirective,
+    UnsubmittedDirective
   ],
   imports: [
     BrowserModule,
